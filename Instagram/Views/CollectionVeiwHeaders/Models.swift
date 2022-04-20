862e5c8ff74f7c1b63e20db137237671b0054c1f
jimin-hash <jimin.park2125@gmail.com>
2022-04-20 22:08:35 +0900

    Feat: 아이콘 추가, 사용자 프로파일 모델 설정

---
+++ Instagram/Views/CollectionVeiwHeaders/Models.swift
@@ -0,0 +1,62 @@
+//
+//  Models.swift
+//  Instagram
+//
+//  Created by Jimin Park on 2022/04/20.
+//
+
+import Foundation
+
+public enum UserPostType {
+    case photo, video
+}
+
+enum Gender {
+    case male, female, other
+}
+
+struct User {
+    let username: String
+    let bio: String
+    let name: (first: String, last: String)
+    let birthData: Date
+    let gender: Gender
+    let counts: UserCount
+    let joinDate: Date 
+}
+
+struct UserCount {
+    let followers: Int
+    let following: Int
+    let posts: Int
+}
+
+public struct UserPost {
+    let identifier: String
+    let postType: UserPostType
+    let thumbnailImage: URL
+    let postURL: URL
+    let caption: String?
+    let likeCount: [PostLike]
+    let comments: [PostComment]
+    let createDate: Date
+    let taggedUsers: [String]
+}
+
+struct PostLike {
+    let username: String
+    let postIdentifier: String
+}
+
+struct CommentLike {
+    let username: String
+    let commentIdentifier: String
+}
+
+struct PostComment {
+    let identifier: String
+    let username: String
+    let text: String
+    let createdDate: Date
+    let likes: [CommentLike]
+}
