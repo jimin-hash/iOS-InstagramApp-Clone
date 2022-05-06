b92f994872f9434390524a5101848a8977fdc468
jimin-hash <jimin.park2125@gmail.com>
2022-05-06 17:14:17 +0900

    Feat: Explore 탭 기능 추가

--- Instagram/Controllers/CoreTabs/ExploreViewController.swift
+++ Instagram/Controllers/CoreTabs/ExploreViewController.swift
@@ -11,12 +11,22 @@ class ExploreViewController: UIViewController {
     
     private let searchBar: UISearchBar = {
         let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
         searchBar.backgroundColor = .secondarySystemBackground
         return searchBar
     }()
     
+    private var models = [UserPost]()
+    
     private var collectionView: UICollectionView?
     
+    private let dimmedView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.isHidden = true
+        view.alpha = 0.0
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,9 +36,12 @@ class ExploreViewController: UIViewController {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
         layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-        layout.itemSize = CGSize(width: view.width/3, height: view.width/3)
+        layout.itemSize = CGSize(width:( view.width-4)/3, height: (view.width-4)/3)
+        layout.minimumLineSpacing = 1
+        layout.minimumInteritemSpacing = 1
         
         collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView?.register(PhotoCollectionViewCell.self, forCellWithReuseIdentifier: PhotoCollectionViewCell.identifier)
         collectionView?.delegate = self
         collectionView?.dataSource = self
         
@@ -36,19 +49,83 @@ class ExploreViewController: UIViewController {
             return
         }
         view.addSubview(collectionView)
+        
+        searchBar.delegate = self
+        
+        view.addSubview(dimmedView)
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapCancelSearch))
+        gesture.numberOfTapsRequired = 1
+        gesture.numberOfTouchesRequired = 1
+        dimmedView.addGestureRecognizer(gesture)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        collectionView?.frame = view.bounds
+        dimmedView.frame = view.bounds
     }
 }
 
 extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return 100
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        return UICollectionViewCell()
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PhotoCollectionViewCell.identifier, for: indexPath) as? PhotoCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(debug: "test")
+        return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        
+//        let model = models[indexPath]
+        let vc = PostViewController(model: models[0])
+        vc.navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+extension ExploreViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        didTapCancelSearch()
+        
+        guard let text = searchBar.text, !text.isEmpty else {
+            return
+        }
+        
+        query(text)
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: self, action: #selector(didTapCancelSearch))
+        dimmedView.isHidden = false
+        UIView.animate(withDuration: 0.2) {
+            self.dimmedView.alpha = 0.4
+        }
+    }
+    
+    private func query(_ text: String) {
+        // Perform the search in the back end
+    }
+    
+    @objc private func didTapCancelSearch() {
+        searchBar.resignFirstResponder()
+        navigationItem.rightBarButtonItem = nil
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.dimmedView.alpha = 0.0
+        }) { done in
+            if done {
+                self.dimmedView.isHidden = true
+            }
+        }
     }
 }
